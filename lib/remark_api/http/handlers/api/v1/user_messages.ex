e913fc4668a7d8dfdb40f0acbb65a030515472ff
Sergey Gernyak <sergeg1990@gmail.com>
2016-04-21 08:14:13 +0300

    Handler, basic impl of processor
    ref #3

--- lib/remark_api/http/handlers/api/v1/user_messages.ex
+++ lib/remark_api/http/handlers/api/v1/user_messages.ex
@@ -3,12 +3,25 @@ defmodule RemarkApi.Http.Handlers.Api.V1.UserMessages do
 
   defp process({"GET", "application/json"}, req, state) do
     login = fetch_binding(req, :login)
-    reply = RemarkApi.Http.Processors.Api.V1.UserMessages.call(login) |> resolve_reply(req)
+    reply = RemarkApi.Http.Processors.Api.V1.UserMessages.get_all_for(login)
+            |> resolve_reply(req)
     {:ok, reply, state}
   end
 
-  defp resolve_reply({:ok, hash}, req) do
+  defp process({"PUT", "application/json"}, req, state) do
+    login = fetch_binding(req, :login)
+    body = fetch_json_request_body(req)
+    reply = RemarkApi.Http.Processors.Api.V1.UserMessages.put_new_for(login, body)
+            |> resolve_reply(req)
+    {:ok, reply, state}
+  end
+
+  defp resolve_reply({:ok, hash}, req) when is_list(hash) do
     make_ok_json_response(req, %{messages: hash})
+  end
+
+  defp resolve_reply({:ok, hash}, req) when is_map(hash) do
+    make_ok_json_response(req, %{message: hash})
   end
 
   defp resolve_reply({:not_found, _}, req) do
--- lib/remark_api/http/processors/api/v1/user_messages.ex
+++ lib/remark_api/http/processors/api/v1/user_messages.ex
@@ -1,15 +1,27 @@
 defmodule RemarkApi.Http.Processors.Api.V1.UserMessages do
-  def call(login) do
-    login
-    |> RemarkApi.User.find_by_login
-    |> process_with_user
+  def get_all_for(login) do
+    do_process(:get, login)
   end
 
-  defp process_with_user(nil) do
+  def put_new_for(login, body) do
+    do_process(:put, login, body)
+  end
+
+  defp do_process(type, login, body \\ %{}) do
+    login
+    |> RemarkApi.User.find_by_login
+    |> process_with_user(type, body)
+  end
+
+  defp process_with_user(nil, _type, _body) do
     {:not_found, %{}}
   end
 
-  defp process_with_user(%RemarkApi.User{} = user) do
+  defp process_with_user(%RemarkApi.User{} = user, :get, _body) do
     {:ok, RemarkApi.Http.Processors.Api.V1.Messages.get_messages(user)}
+  end
+
+  defp process_with_user(%RemarkApi.User{} = user, :put, body) do
+    {:ok, %{some: "another"}}
   end
 end
