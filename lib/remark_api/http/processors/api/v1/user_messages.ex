68abf0c9fae48b25cdff0e48e57cc6b10fc255e3
Sergey Gernyak <sergeg1990@gmail.com>
2016-04-26 08:38:41 +0300

    Implemented endpoint to post new messages
    close #3

--- lib/remark_api/http/processors/api/v1/user_messages.ex
+++ lib/remark_api/http/processors/api/v1/user_messages.ex
@@ -1,27 +1,46 @@
 defmodule RemarkApi.Http.Processors.Api.V1.UserMessages do
+  import RemarkApi.Helper
+
+  alias RemarkApi.{User, Repo, Message}
+
   def get_all_for(login) do
     do_process(:get, login)
   end
 
   def put_new_for(login, body) do
-    do_process(:put, login, body)
+    do_process(:put, login, fetch_msg_params(body))
   end
 
-  defp do_process(type, login, body \\ %{}) do
+  defp do_process(type, login, msg_params \\ %{}) do
     login
     |> RemarkApi.User.find_by_login
-    |> process_with_user(type, body)
+    |> process_with_user(type, msg_params)
   end
 
-  defp process_with_user(nil, _type, _body) do
+  defp fetch_msg_params(body) do
+    body |> Map.get(:message, %{})
+  end
+
+  defp process_with_user(nil, _type, _msg_params) do
     {:not_found, %{}}
   end
 
-  defp process_with_user(%RemarkApi.User{} = user, :get, _body) do
+  defp process_with_user(%RemarkApi.User{} = user, :get, _msg_params) do
     {:ok, RemarkApi.Http.Processors.Api.V1.Messages.get_messages(user)}
   end
 
-  defp process_with_user(%RemarkApi.User{} = user, :put, body) do
-    {:ok, %{some: "another"}}
+  defp process_with_user(%RemarkApi.User{} = user, :put, msg_params) do
+    %Message{user_id: user.id}
+    |> Message.changeset_create(msg_params)
+    |> Repo.insert
+    |> process_by_insert_result
+  end
+
+  defp process_by_insert_result({:ok, message}) do
+    hash = RemarkApi.Serializers.Message.cast(message)
+    {:ok, hash}
+  end
+  defp process_by_insert_result({:error, changeset}) do
+    {:error, pretty_errors(changeset)}   
   end
 end
--- test/lib/remark_api/http/processors/api/v1/user_messages_test.exs
+++ test/lib/remark_api/http/processors/api/v1/user_messages_test.exs
@@ -12,4 +12,11 @@ defmodule RemarkApi.Htto.Processors.Api.V1.UserMessagesTest do
     assert {:ok, messages} = res
     assert Enum.count(messages) == 1
   end
+
+  test "put new successfully" do
+    user = create(:user)
+    body = %{message: %{body: "Some message"}}
+    res = RemarkApi.Http.Processors.Api.V1.UserMessages.put_new_for(user.login, body)
+    assert {:ok, _message} = res
+  end
 end
