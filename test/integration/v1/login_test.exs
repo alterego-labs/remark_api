3c1c21610e44e043a7e2ec2dcfc0c46fecd7f5b4
Sergey Gernyak <sergeg1990@gmail.com>
2016-06-04 16:53:04 +0300

    Added integrations tests for login endpoint

--- test/integration/v1/login_test.exs
+++ test/integration/v1/login_test.exs
@@ -1,8 +1,18 @@
 defmodule V1.LoginTest do
   use TestCaseWithDbSandbox
 
-  test "failure if credentials are incorrect" do
+  test "failure if request params are malformed" do
+    res = ApiCall.post("v1/login", [body: %{}])
+    assert res.status_code == 422
+  end
+
+  test "failure if login in credentials is empty" do
     res = ApiCall.post("v1/login", [body: %{user: %{login: ""}}])
     assert res.status_code == 422
+  end
+
+  test "success if parameters are valid and not empty" do
+    res = ApiCall.post("v1/login", [body: %{user: %{login: "sergio"}}])
+    assert res.status_code == 200
   end
 end
