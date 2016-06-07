5e27e9039421fc0db1d9fe70a2e5756fae425bee
Sergey Gernyak <sergeg1990@gmail.com>
2016-06-07 15:02:14 +0300

    Refactored tests
    I moved generating valid user login to the shared context, so each test
    can use it without copy-paste.

--- test/integration/v2/register_test.exs
+++ test/integration/v2/register_test.exs
@@ -7,16 +7,12 @@ defmodule V2.RegisterTest do
   end
 
   test "failure if some validations didn't pass" do
-    # user = create(:user)
     res = ApiCall.post("v2/register", [body: %{user: %{login: "ah", password: "password"}}])
     assert res.status_code == 422
   end
 
   test "returns 200 if all info is valid" do
-    login = Faker.Lorem.characters(%Range{first: 3, last: 10}) |> to_string |> String.downcase
-    IO.inspect login
-    res = ApiCall.post("v2/register", [body: %{user: %{login: login, password: "password"}}])
-    IO.inspect res.body
+    res = ApiCall.post("v2/register", [body: %{user: %{login: random_valid_user_login, password: "password"}}])
     assert res.status_code == 200
   end
 end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -3,7 +3,7 @@ defmodule RemarkApi.Factory do
 
   def factory(:user) do
     %RemarkApi.User{
-      login: Faker.Internet.user_name
+      login: random_valid_user_login
     }
   end
 
@@ -16,5 +16,11 @@ defmodule RemarkApi.Factory do
   def with_message(%RemarkApi.User{} = user) do
     create(:message, user: user)
     user
+  end
+
+  def random_valid_user_login do
+    Faker.Lorem.characters(%Range{first: 3, last: 10})
+    |> to_string
+    |> String.downcase
   end
 end
